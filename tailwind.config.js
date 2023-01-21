e74425ce96d7374203802d19d2831e93de5f0f1f
ecemcetin <dev.ecemcetin@gmail.com>
2023-01-21 21:37:10 +0300

    branch fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      bg: '#231955',
-      white: '#ffffff',
+      bg: "#231955",
+      white: "#ffffff",
     },
     extend: {},
   },
   plugins: [],
-}
+};
